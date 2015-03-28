d1177360eebf244fdff74437bd1fbb8805876263
sh4nks <sh4nks7@gmail.com>
2015-03-28 14:44:15 +0100

    WIP - dotfiles overhaul

--- emacs.d/modules/init-pkgbuild.el
+++
@@ -1,5 +0,0 @@
-(autoload 'pkgbuild-mode "pkgbuild-mode.el" "PKGBUILD Mode." t)
-(setq auto-mode-alist (append '(("/PKGBUILD$" . pkgbuild-mode))
-                              auto-mode-alist))
-
-(provide 'init-pkgbuild)
